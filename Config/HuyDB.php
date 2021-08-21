425976ee78f0d73c4276fe51587dbb042652e24f
nhathuy7996 <nhat.huy.7996@gmail.com>
2021-08-21 21:55:50 +0700

    add fetch_secu

--- Config/HuyDB.php
+++ Config/HuyDB.php
@@ -7,7 +7,7 @@ class HuyDB
 	private $hostname = 'localhost',
 			$username = 'root',
 			$password = '',
-			$dbname = 'web';
+			$dbname = 'bloghuynn';
 
 	// Biến lưu trữ kết nối
 	private static $cn = NULL;
@@ -35,6 +35,7 @@ class HuyDB
 	{
 		if(empty(self::$cn))
 			self::$cn = mysqli_connect($this->hostname, $this->username, $this->password, $this->dbname);
+		return self::$cn;
 	}
 
 	// Hàm ngắt kết nối
@@ -49,52 +50,101 @@ class HuyDB
 	// Hàm truy vấn
 	public function query($sql = null) 
 	{		
-	    if (self::$cn)
-	    {
-			return mysqli_query(self::$cn, $sql);
-	    }
+	    
+		return mysqli_query($this->connect(), $sql);
+	    
 	}
 
 	// Hàm đếm số hàng
 	public function num_rows($sql = null) 
 	{
-		if (self::$cn)
+		
+		$query = mysqli_query($this->connect(), $sql);
+		if ($query)
 		{
-			$query = mysqli_query(self::$cn, $sql);
-			if ($query)
+			$row = mysqli_num_rows($query);
+			return $row;
+		}	
+			
+	}
+	public function fetch_secu($sql,$types = null,$params = null, $getmulti = true) {
+
+		$stmt = $this->connect()->prepare($sql);
+		$stmt->bind_param($types, ...$params);
+	
+		if(!$stmt->execute()) return false;
+		$res = $stmt->get_result();
+	
+		if ($getmulti == 0)
+		{
+			// Lấy nhiều dữ liệu gán vào mảng
+			$data = array();
+			while ($row = $res->fetch_object())
 			{
-				$row = mysqli_num_rows($query);
-				return $row;
-			}	
-		}		
+				$data[] = $row;
+			}
+			
+		}
+		else 
+		{
+			// Lấy một hàng dữ liệu gán vào biến
+			$data = $res->fetch_object();
+
+		}
+			
+		return $data;
 	}
 
+	public function fetch_obj($sql = null, $type = 0)
+	{
+		
+		$query = mysqli_query($this->connect(), $sql);
+		if ($query)
+		{
+			if ($type == 0)
+			{
+				// Lấy nhiều dữ liệu gán vào mảng
+				$data = array();
+				while ($obj= mysqli_fetch_object($query))
+				{
+					$data[] = $obj;
+				}
+				return $data;
+			}
+			else if ($type == 1)
+			{
+				// Lấy một hàng dữ liệu gán vào biến
+				$data = mysqli_fetch_object($query);
+				return $data;
+			}
+		}		
+		
+	}
 	// Hàm lấy dữ liệu
 	public function fetch_assoc($sql = null, $type = 0)
 	{
-		if (self::$cn)
+		
+		$query = mysqli_query($this->connect(), $sql);
+		if ($query)
 		{
-			$query = mysqli_query(self::$cn, $sql);
-			if ($query)
+			if ($type == 0)
 			{
-				if ($type == 0)
+				// Lấy nhiều dữ liệu gán vào mảng
+				$data = array();
+				while ($row = mysqli_fetch_assoc($query))
 				{
-					// Lấy nhiều dữ liệu gán vào mảng
-					$data = array();
-					while ($row = mysqli_fetch_assoc($query))
-					{
-						$data[] = $row;
-					}
-					return $data;
+					$data[] = $row;
 				}
-				else if ($type == 1)
-				{
-					// Lấy một hàng dữ liệu gán vào biến
-					$data = mysqli_fetch_assoc($query);
-					return $data;
-				}
-			}		
-		}
+				return $data;
+			}
+			else if ($type == 1)
+			{
+				// Lấy một hàng dữ liệu gán vào biến
+				$data = mysqli_fetch_assoc($query);
+				return $data;
+			}
+		}		
+		
 	}
 
 	public function Security($sql = null){
@@ -104,28 +154,26 @@ class HuyDB
 	// Hàm lấy ID cao nhất
 	public function insert_id()
 	{
-		if (self::$cn)
+		
+		$count = mysqli_insert_id($this->connect());
+		if ($count == '0')
 		{
-			$count = mysqli_insert_id(self::$cn);
-			if ($count == '0')
-			{
-				$count = '1';
-			}
-			else
-			{
-				$count = $count;
-			}
-			return $count;
+			$count = '1';
 		}
+		else
+		{
+			$count = $count;
+		}
+		return $count;
+		
 	}
 
 	// Hàm charset cho database
 	public function set_char($uni)
 	{
-		if (self::$cn)
-		{
-			mysqli_set_charset(self::$cn, $uni);
-		}
+		
+		mysqli_set_charset($this->connect(), $uni);
+		
 	}
 }
 
